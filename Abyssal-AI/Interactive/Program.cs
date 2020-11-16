2d31e7568a85f9c3e3f0510c53475eb466ca2f45
hackerbuddy229 <rasmusb229@gmail.com>
2020-11-16 13:11:08 +0100

    ✨Generated basic interaction project & core library
    Building Archlinux 5.9.8 Rolling .net core 3.1

---
+++ Abyssal-AI/Interactive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abyssal_AI.Interactive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
