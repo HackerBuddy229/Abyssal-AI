9790e9b266f7a693894f8eba34bba166f67ac626
Rasmus Bengtsson <rasmusb229@gmail.com>
2021-01-18 13:51:04 +0100

    Merged unsafe code for migration

---
+++ AbyssalAI/Core/Neurons/Neuron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbyssalAI.Core.Neurons
+{
+    public class Neuron: FiringNeuron
+    {
+        public Neuron() : base()
+        {
+            
+        }
+
+	public void hello(string test) {
+		Console.WriteLine(test);
+	}
+    }
+}
